c70626c2b2f90b30cee34ce1389aa595c6d630b5
Glitch (boiled-plucky-stealer) <none>
2025-02-10 23:00:55 +0000

    ↘️💭 Updated with Glitch

--- public/index.js
+++ public/index.js
@@ -2,16 +2,16 @@ async function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("/login", {
+    const res = await fetch("/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
     });
 
-    const data = await response.json();
-    document.getElementById("message").innerText = data.message;
-
+    const data = await res.json();
     if (data.success) {
-        window.location.href = "/home.html";
+        window.location.href = "home.html";
+    } else {
+        document.getElementById("error").innerText = "Login inválido!";
     }
 }
