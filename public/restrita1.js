83c81c9d4ca77f8cc921777a8483f2c0ce5d7d98
viniciuspolegatto <93634376+viniciuspolegatto@users.noreply.github.com>
2025-02-10 23:11:37 -0300

    Update restrita1.js

--- public/restrita1.js
+++ public/restrita1.js
@@ -1,3 +1,24 @@
+// Verifica se o usuário está autenticado antes de carregar o conteúdo da página
+async function checkAuth() {
+  try {
+    const response = await fetch('/check-auth', { method: 'GET' });
+    const data = await response.json();
+
+    if (!data.authenticated) {
+      console.log('Usuário não autenticado, redirecionando para login');
+      window.location.href = '/'; // Redireciona para a página de login
+    } else {
+      console.log('Usuário autenticado, carregando conteúdo da página restrita1');
+    }
+  } catch (error) {
+    console.error('Erro ao verificar autenticação:', error);
+  }
+}
+
+// Verifica a autenticação ao carregar a página
+checkAuth();
+
+// Logout
 document.getElementById('logoutButton').addEventListener('click', () => {
   fetch('/logout', { method: 'GET' })
     .then(() => {
@@ -6,6 +27,7 @@ document.getElementById('logoutButton').addEventListener('click', () => {
     .catch(error => console.error('Erro ao fazer logout:', error));
 });
 
+// Redireciona para a página home
 document.getElementById('homeButton').addEventListener('click', () => {
   window.location.href = '/home'; // Redireciona para a página home
 });
